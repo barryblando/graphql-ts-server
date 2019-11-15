afd524fd2ad703423dc8a9d1954ae3ff5703ac04
barryblando <barryblando@gmail.com>
2019-11-15 22:10:46 +0800

    uncomment line 17-19

--- src/tasks/db/seed.ts
+++ src/tasks/db/seed.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata'
 
 import { createConnection, Connection } from 'typeorm'
 
-import { typeOrmConfig } from '../../../ormConfig'
+import { typeOrmConfig } from '../../ormConfig'
 import { User } from '../../entity/User'
 
 const startServer = async () => {
@@ -15,7 +15,8 @@ const startServer = async () => {
 	console.log('Inserting a new user into the database...')
 	const user = new User()
 	user.firstName = 'Timber'
-	user.age = 25
+	user.lastName = 'Land'
+	user.email = 'timber@timber.com'
 	await connection.manager.save(user)
 	console.log('Saved a new user with id: ' + user.id)
 
