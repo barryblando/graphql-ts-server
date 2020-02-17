4936ddd7d27b456b55cafcdd44c8e68bbc87def0
Barry Blando <barryblando@gmail.com>
2020-02-17 22:51:32 +0800

    add isAuth for frontend test

--- src/modules/hello/Hello.ts
+++ src/modules/hello/Hello.ts
@@ -1,8 +1,10 @@
-import { Resolver, Query } from 'type-graphql'
+import { Resolver, Query, UseMiddleware } from 'type-graphql'
+import { isAuth } from '../middleware/isAuth'
 
 @Resolver()
 export class HelloResolver {
 	@Query(returns => String)
+	@UseMiddleware(isAuth)
 	hello() {
 		return 'Hello World!'
 	}
