59368cecbddb746454c1ef21954ff48360410007
Barry Blando <barryblando@gmail.com>
2020-02-17 22:54:37 +0800

    return back confirmation url

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,7 +1,10 @@
 import nodemailer from 'nodemailer'
 
 // async..await is not allowed in global scope, must use a wrapper
-export async function sendEmail(email: string, url: string) {
+export async function sendEmail(
+	email: string,
+	url: string,
+): Promise<{ messageId: string; messageUrl: string | boolean }> {
 	// Generate test SMTP service account from ethereal.email
 	// Only needed if you don't have a real mail account for testing
 	const testAccount = await nodemailer.createTestAccount()
@@ -26,10 +29,15 @@ export async function sendEmail(email: string, url: string) {
 		html: `<a href="${url}">${url}</a>`, // html body
 	})
 
-	console.log('Message sent: %s', info.messageId)
+	// console.log('Message sent: %s', info.messageId)
 	// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
 	// Preview only available when sending through an Ethereal account
-	console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+	// console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
 	// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+	return {
+		messageId: info.messageId,
+		messageUrl: nodemailer.getTestMessageUrl(info),
+	}
 }
