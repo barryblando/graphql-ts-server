88f46482b019360be3a0331bb40473ba3bdc520b
Barry Blando <barryblando@gmail.com>
2020-02-17 22:50:21 +0800

    return confirmation url for dev purposes

--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -4,6 +4,7 @@ import { RegisterInput } from './register/RegisterInput'
 import { rateLimit } from '../middleware/rateLimit'
 import { sendEmail } from '../../utils/sendEmail'
 import { createConfirmationUrl } from '../../utils/createConfirmationUrl'
+import { RegisterResponse } from '../../graphql-types/RegisterResponse'
 
 // Specifying User in Resolver parameter so we can know where this name field resolver are resolving for so its resolving for User
 @Resolver()
@@ -13,13 +14,14 @@ export class RegisterResolver {
 	// 	return `${parent.firstName} ${parent.lastName}`
 	// }
 
-	@Mutation(returns => User, { description: 'Register reasonably', nullable: true })
+	@Mutation(returns => RegisterResponse, { description: 'Register reasonably', nullable: true })
 	@UseMiddleware(rateLimit()) // add rate-limiting to prevent spamming, keep track of that person's ip
 	async register(
 		@Arg('data', { validate: true }) { firstName, lastName, email, password }: RegisterInput,
-	): Promise<User> {
+	): Promise<RegisterResponse> {
 		// const hashedPassword = await bcrypt.hash(password, 12)
 		let user: User
+		let confirmationResponse
 
 		try {
 			user = await User.create({
@@ -29,11 +31,11 @@ export class RegisterResolver {
 				password,
 			}).save()
 
-			await sendEmail(email, await createConfirmationUrl(user.id))
+			confirmationResponse = await sendEmail(email, await createConfirmationUrl(user.id))
 		} catch (err) {
 			console.log(err)
 		}
 
-		return user
+		return { user, ...confirmationResponse }
 	}
 }
